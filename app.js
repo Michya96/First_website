dff84a5b7055a913d7592134eaa057e378a6031d
michya96 <michja144@gmail.com>
2022-08-31 02:44:35 +0200

    Adding responsiveness

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const sections = document.querySelectorAll('.section');
 const sectionBtns = document.querySelectorAll('.controls'); //Selects the group of all buttons
 const sectionBtn = document.querySelectorAll('.control'); //Selects each button in the header
 const allSections = document.querySelector('.main-content');
+const themeBtn = document.querySelector('.logo')
 
 function ChangingPage(){
     for(let i = 0; i < sectionBtn.length; i++){
@@ -19,12 +20,22 @@ function ChangingPage(){
         })
     }
 
+    
 
 }
 
+function ChangingTheme(){
+    themeBtn.addEventListener('click', () =>{
+        console.log('heh')
+        let element = document.body;
+        console.log(element);
+        element.classList.toggle('light-mode');
+    })
+}
 function RemovingActiveTag(){
     let currentBtn = document.querySelector('.active-btn');
     currentBtn.className = currentBtn.className.replace('active-btn', '');
 }
 
 ChangingPage();
+ChangingTheme();
